75e310293452c5278fd446ea45cf70ef12b8b64e
Giulio <giuliovolletti@hotmail.com>
2020-11-30 15:15:01 +0100

    commit1

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(
+  function () {
+    console.log("hello world");
+    // Esercizio: creare una to-do list, come quella fatta insieme stamattina, utilizzando Handlebars come templating engine
+
+    // collegamento
+    var source = $('#entry-template').html();
+    console.log(source);
+    var template = Handlebars.compile(source);
+
+    var toDoList = [
+      "cucinare",
+      "fare benzia",
+      "fare la spesa",
+      "comprare il giornale"
+    ];
+
+    for (var i = 0; i < toDoList.length; i++) {
+      var listItem ={
+        toHtml: toDoList[i]
+      };
+      var html = template(listItem);
+      $('#list').append(html)
+      console.log(html);
+    }
+
+
+  }
+);
