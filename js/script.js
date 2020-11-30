206b34558efb7e5be919cb3002a0789280d76ff6
Giulio <giuliovolletti@hotmail.com>
2020-11-30 16:48:57 +0100

    commit6

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ $(document).ready(
 
     // collegamento
     var source = $('#entry-template').html();
-    console.log(source);
+    // console.log(source);
     var template = Handlebars.compile(source);
+
 
     var toDoList = [
       "cucinare",
@@ -15,36 +16,38 @@ $(document).ready(
       "comprare il giornale"
     ];
 
+    // aggiunta list item in html
     for (var i = 0; i < toDoList.length; i++) {
-      var listItem ={
+      var listItem = {
         toHtml: toDoList[i]
       };
       var html = template(listItem);
       $('#list').append(html)
     }
 
+    // rimozione di list item con click
     $(document).on( "click", '.fa-check-circle', function(){
       $(this).parent().remove();
     }
     );
 
+    // inserimento nuovo list item con enter
     $("input").keyup(
       function(){
         // console.log(event.which);
         if (event.which == 13) {
-          console.log($("input").val());
+          // console.log($("input").val());
           var newItem = $("input").val();
           var newobject = {
             toHtml : newItem,
           }
-
           var htmlNew = template(newobject);
-          $('#list').append(htmlNew)
-
-
+          $('#list').append(htmlNew);
+          // cancella valore in input
+          $("input").val("");
         }
-
       }
     );
+
   }
 );
